6a3eeb0a66885cc4a222e21311abbc4df47a1583
yazirobe227 <yazirobe@gmail.com>
2025-05-23 21:18:58 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-    function showModal(mediumSrc, largeSrc) {
-      document.getElementById('modalImage').src = mediumSrc;
-      document.getElementById('enlargeLink').href = largeSrc;
-      document.getElementById('modal').style.display = 'block';
-    }
+function showModal(mediumSrc, largeSrc) {
+  const modal = document.getElementById('modal');
+  const modalImage = document.getElementById('modalImage');
+  const enlargeLink = document.getElementById('enlargeLink');
 
-    function closeModal() {
-      document.getElementById('modal').style.display = 'none';
-    }
+  modalImage.src = mediumSrc;
+  enlargeLink.href = largeSrc;
+
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  const modal = document.getElementById('modal');
+  const modalImage = document.getElementById('modalImage');
+  const enlargeLink = document.getElementById('enlargeLink');
+
+  modal.style.display = 'none';
+  modalImage.src = ""; // 表示画像を初期化
+  enlargeLink.href = "#"; // リンクも初期化
+}
