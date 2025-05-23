15c5600033972fa546d7b1b8c5dda643b02cc94b
yazirobe227 <yazirobe@gmail.com>
2025-05-23 12:43:37 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+  <script>
+    function showModal(mediumSrc, largeSrc) {
+      document.getElementById('modalImage').src = mediumSrc;
+      document.getElementById('enlargeLink').href = largeSrc;
+      document.getElementById('modal').style.display = 'block';
+    }
+
+    function closeModal() {
+      document.getElementById('modal').style.display = 'none';
+    }
